344196ffad35ea373c1cf730e514404de90a8ba7
Bram de Leeuw <bram-de-leeuw@hotmail.com>
2017-01-10 10:07:26 +0100

    added pageslices delete action in lack of a alternative

--- code/gridfield/GridFieldConfig_PageSlices.php
+++ code/gridfield/GridFieldConfig_PageSlices.php
@@ -32,7 +32,7 @@ class GridFieldConfig_PageSlices extends GridFieldConfig
         $this->addComponent(new GridFieldDataColumns());
         $this->addComponent(new VersionedDataObjectDetailsForm());
         $this->addComponent(new GridFieldEditButton());
-        $this->addComponent(new VersionedGridFieldDeleteAction());
+        $this->addComponent(new PageSlices_VersionedGridFieldDeleteAction());
         $this->addComponent(new VersionedGridFieldOrderableRows($sortField));
         $this->addComponent($multiClassComponent = new GridFieldAddNewMultiClass());
         $this->addComponent($pagination = new GridFieldPaginator($itemsPerPage));
@@ -54,5 +54,36 @@ class GridFieldConfig_PageSlices extends GridFieldConfig
             $out[$class] = $class::singleton()->i18n_singular_name();
         }
         return $out;
+    }
+}
+
+/**
+ * Class VersionedGridFieldDeleteAction
+ *
+ * Extend the delete action with a versioned delete
+ * This class is temporarily included in the boilerplate until
+ */
+class PageSlices_VersionedGridFieldDeleteAction extends GridFieldDeleteAction
+{
+
+    /**
+     * Delete the object form both live and stage
+     *
+     * @param GridField $gridField
+     * @param string $actionName
+     * @param mixed $arguments
+     * @param array $data
+     * @throws ValidationException
+     */
+    public function handleAction(GridField $gridField, $actionName, $arguments, $data)
+    {
+        if ($item = $gridField->getList()->byID($arguments['RecordID'])) {
+            if (!$item->canDelete()) {
+                throw new ValidationException(
+                    _t('GridFieldAction_Delete.DeletePermissionsFailure', "No delete permissions"), 0);
+            }
+            $item->deleteFromStage('Live');
+            $item->delete();
+        }
     }
 }
